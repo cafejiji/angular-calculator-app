9586f221464303f51c5e2450a0b24871265187d0
cafejiji <cafejiji.0118@gmail.com>
2021-12-26 18:02:55 +0100

    enhancement / eslint / prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-calculator-app';
   subDisplayText = '';
   mainDisplayText = '';
+  values = new Array<string>();
+  operators = new Array<string>('+', '-', '*', '/', '.');
 
   pressKey(key: string) {
-    this.mainDisplayText += key;
+    if (this.operators.includes(key) && (this.values.length === 0 || this.operators.includes(this.values[this.values.length - 1]))) {
+      return;
+    }
+
+    this.values.push(key);
+    this.mainDisplayText = this.values.join('');
   }
+
   allClear() {
     this.mainDisplayText = '';
     this.subDisplayText = '';
+    this.values = [];
   }
+
   getAnswer() {
-    this.subDisplayText = this.mainDisplayText;
-    this.mainDisplayText = eval(this.mainDisplayText);
+    if (this.operators.includes(this.values[this.values.length - 1])) {
+      this.values.pop();
+    }
+
+    this.subDisplayText = this.values.join('');
+    this.mainDisplayText = eval(this.subDisplayText);
+    this.values = [];
+    this.values.push(this.mainDisplayText);
   }
 }
